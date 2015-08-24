00ded5f0b5f5cf35470a641f854578d672d8d2ca
Jacob Sanders <j.q.sanders@gmail.com>
2015-08-24 14:39:17 -0600

    created models.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth import User
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField(null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
+    runtime = models.CharField(max_length=40, null=True, blank=True)
+    genre = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    starring = models.TextField(null=True, blank=True)
+    written_by = models.TextField(null=True, blank=True)
+    directed_by = models.CharField(max_length=255, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+    voters = models.ManyToManyField('User', related_name='votes', blank=True)
+    user = models.ManyToManyField('User')
+
+    def __unicode__(self):
+        return self.title
+
+
+class WatchEvent(models.Model):
+    event_name = models.CharField(max_length=255)
+    date_and_time = models.DateTimeField()
+    url_slug = models.SlugField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    description = models.TextField(null=True, blank=True)
+    movie_watched = models.ForeignKey('Movie')
+    user_present = models.ForeignKey('User')
+    group_organizing = models.OneToOneField('WatchRoom')
+
+    def __unicode__(self):
+        return self.event_name
+
+
+class WatchRoom(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    users = models.ManyToManyField('User')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
