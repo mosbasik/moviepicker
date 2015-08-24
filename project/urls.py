48e2e5bfd6657ca77dcb8f33097ae50264483fbd
azuretower <ryan.merrill215@gmail.com>
2015-08-24 14:43:46 -0600

    initial file set up

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,28 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', 'main.views.front', name='front'),
+
+
+    url(r'^password/reset/$',
+        django.contrib.auth.views.password_reset,
+        {"template_name": "password_reset/password_reset_form.html"},
+        name="password_reset"
+        ),
+    url(r'^password/reset/done/$',
+        django.contrib.auth.views.password_reset_done,
+        {"template_name": "password_reset/password_reset_form_done.html"},
+        name="password_reset_done"),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+        django.contrib.auth.views.password_reset_confirm,
+        {"template_name": "password_reset/password_reset_confirm.html"},
+        name="password_reset_confirm"),
+    url(r'^password/reset/confirm/done/$',
+        django.contrib.auth.views.password_reset_complete,
+        {"template_name": "password_reset/password_reset_complete.html"},
+        name="password_reset_complete"),
 ]
