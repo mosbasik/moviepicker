cf6aa02d9047261a7d284c8531d9dba774ef1d99
Jacob Sanders <j.q.sanders@gmail.com>
2015-08-24 19:44:03 -0600

    script can now hit the api and parse the returning json, storing it in the db.

--- scripts/populate_movies.py
+++ scripts/populate_movies.py
@@ -3,11 +3,12 @@ import sys, os
 import requests
 import json
 from StringIO import StringIO
+import unicodedata
 
-# sys.path.append('..')
-# os.environ.setdefault("DJANGO_SETTING_MODULE", "project.settings")
+sys.path.append('..')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-# from main.models import Movie
+from main.models import Movie
 # from django.conf import settings
 
 # from django.core.files import File
@@ -21,26 +22,23 @@ omdb_api_url = requests.get('http://www.omdbapi.com/?i=%s&plot=full&r=json'
                             % movie_id)
 
 response_dict = omdb_api_url.json()
-# print response_dict
+
+# import pprint
+# pprint.pprint(response_dict)
 # print response_dict['Plot']
 
-for data in response_dict:
-    movie, created = Movie.objects.get_or_create(imdb_id=data.get('imdbID'))
 
-    movie.title = data.get('Title')
-    movie.year = data.get('Year')
-    movie.imdb_rating = data.get('imdbRating')
-    movie.runtime = data.get('Runtime')
-    movie.genre = data.get('Genre')
-    movie.description = data.get('Plot')
-    movie.starring = data.get('Actors')
-    movie.written_by = data.get('Writer')
-    movie.directed_by = data.get('Director')
+movie, created = Movie.objects.get_or_create(imdb_id=response_dict['imdbID'])
 
-    movie.save()
-    print movie
+movie.title = response_dict['Title']
+movie.year = response_dict['Year']
+movie.imdb_rating = response_dict['imdbRating']
+movie.runtime = response_dict['Runtime']
+movie.genre = response_dict['Genre']
+movie.description = response_dict['Plot']
+movie.starring = response_dict['Actors']
+movie.written_by = response_dict['Writer']
+movie.directed_by = response_dict['Director']
 
-
-
-
-
+movie.save()
+print movie
