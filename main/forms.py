b9d0642b3239a8e787f60d8f2bc8476ade669bb5
Peter Henry <phenry@caltech.edu>
2015-09-01 10:49:56 -0600

    registration uses django form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
-from django import forms
-from django.forms import ModelForm
 from datetimewidget.widgets import DateTimeWidget
+from django import forms
+from django.contrib.auth.models import User
+# from django.forms import ModelForm
+
 from main.models import Movie, Event, Group
 
 
@@ -72,12 +74,56 @@ class EventCreationForm(forms.Form):
     )
 
 
-    # class Meta:
-    #     model = WatchEvent
-    #     fields = ['event_name', 'date_and_time', 'description']
-    #     widgets = {
-    #         'datetime': DateTimeWidget(
-    #             # attrs={'class': 'form-control'},
-    #             usel10n=True,
-    #             bootstrap_version=3)
-    #         }
+class UserCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+    }
+
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            }))
+
+    email = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            }))
+
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+            }))
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+            }))
+
+    def clean_password2(self):
+            password1 = self.cleaned_data.get('password1', None)
+            password2 = self.cleaned_data.get('password2', None)
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch'
+                )
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
--- main/user_auth.py
+++ main/user_auth.py
@@ -3,61 +3,93 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+
 
 from main.models import Group
+from main.forms import UserCreationForm
 
 
 def login(request):
+
+    # if a user is already logged in, redirect to the front page
     if request.user.is_authenticated():
             return redirect('front')
 
+    # load a blank registration form to context in case they want to register
     context = {'user_create_form': UserCreationForm()}
 
     if request.method == 'POST':
 
+        # if the POST request was a submission of the login form
         if request.POST['type'] == 'login':
-            username = request.POST['username']
-            password = request.POST['password']
 
-            user = authenticate(username=username, password=password)
+            # attempt to authenticate the user
+            user = authenticate(username=request.POST['username'],
+                                password=request.POST['password'])
 
+            # if the user is found in the database
             if user is not None:
-                if user.is_active:
-                    auth_login(request, user)
 
+                # and if the user's account is active
+                if user.is_active:
+
+                    # then log the user in and redirect to front page
+                    auth_login(request, user)
                     if request.user.is_authenticated():
                         return redirect('front')
+
+                # if the user's account is not active
                 else:
+
+                    # load an error message to context and reload page
                     context['error'] = 'Your account has been disabled'
                     return render(request, 'login.html', context)
 
+            # if the user is not found in the database
             else:
-                context['error'] = 'invalid username or password'
+
+                # load an error message to context and reload page
+                context['error'] = 'Invalid username or password'
                 return render(request, 'login.html', context)
 
-        else:
-            full_user_create_form = UserCreationForm(request.POST)
+        # if the POST request was a submission of the registration form
+        elif request.POST['type'] == 'create_user':
 
-            if full_user_create_form.is_valid():
+            # save a copy of the filled registration form
+            filled_user_creation_form = UserCreationForm(request.POST)
 
-                user = full_user_create_form.save()
-                user.email = request.POST['email']
-                user.save()
+            # if the filled form is valid
+            if filled_user_creation_form.is_valid():
 
-                room = Group.objects.get(name='Universal')
-                room.users.add(user)
+                # create a new user with the form information (the email field
+                # has to be saved manually because although Django renders an
+                # email field by default, it doesn't actually save it by
+                # default)
+                user = filled_user_creation_form.save()
+                # user.email = request.POST['email']
+                # user.save()
 
-                # group = Group.objects.get(name='users')
-                # group.user_set.add(user)
+                # authenticate the new user against the database (a formality)
+                user = authenticate(username=request.POST['username'],
+                                    password=request.POST['password1'])
 
-                user = authenticate(username=user.username, password=request.POST['password1'])
+                # log the new user into the site
                 auth_login(request, user)
 
+                # get the "world" group and add the new user to it
+                group = Group.objects.get(name='World')
+                group.users.add(user)
+
+                # redirect user to the front page
                 return redirect('front')
+
+            # if the filled form is invalid
             else:
-                context['user_create_form'] = full_user_create_form
+
+                # load invalid form to context to be passed back for editing
                 context['error_on_create'] = True
+                context['user_create_form'] = filled_user_creation_form
 
     return render(request, 'login.html', context)
 
